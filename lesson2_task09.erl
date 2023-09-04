2f98c984333c7246153c609d967f5af93a670c9e
ovgofman <o.v.gofman@gmail.com>
2023-09-04 12:20:09 +0300

    Create lesson2_task09.erl

---
+++ lesson2_task09.erl
@@ -0,0 +1,19 @@
+%%%-------------------------------------------------------------------
+%%% @author alex
+%%% @copyright (C) 2023
+%%% @doc
+%%%
+%%% @end
+%%% Created : 19. Aug 2023 11:26 PM
+%%%-------------------------------------------------------------------
+-module(lesson2_task09).
+-author("alex").
+
+%% API
+-export([
+  pack/1    % [a,a,a,b,c,c] -> [[a,a,a],[b],[c,c]]
+]).
+
+%% @spec 9. Pack into nested list. TODO: Figure it out
+%% [a,a,a,b,c,c] -> [[a,a,a],[b],[c,c]]
+pack(L) -> L.
