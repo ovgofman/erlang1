6acb3e1f3c13b8db82f24615a9010f9c9e23c518
ovgofman <o.v.gofman@gmail.com>
2023-09-05 22:00:39 +0300

    Remove dependenies on helper_funcs.erl

--- lesson2_task06.erl
+++ lesson2_task06.erl
@@ -15,7 +15,6 @@
 ]).
 -import(lesson2_task04, [len/1]).
 -import(lesson2_task05, [reverse/1]).
--import(helper_funcs, [take/2, compare/2]).
 
 %% @spec 6. Returns 'true' if palindrome, 'false' otherwise
 is_palindrome(L) ->
@@ -23,3 +22,26 @@ is_palindrome(L) ->
   L2 = take(L, LL),
   R2 = take(reverse(L), LL),
   compare(L2, R2).
+
+%% @spec Return first N elems.
+%% take([1,2,3],2) -> [1,2]
+take(L, N) -> loop(L, [], N).
+loop(L, R, A) ->
+  LL = len(L),
+  if
+    A > LL -> [];
+    A =< 0 -> reverse(R);
+    true -> case L of
+              [] -> R;
+              [H | T] -> loop(T, [H | R], A - 1)
+            end
+  end.
+
+%% @spec Compare lists.
+%% compare([1,2],[1,2]) -> true
+compare(L, R) ->
+  case {L, R} of
+    {[H | []], [H2 | []]} when H =:= H2 -> true;
+    {[H | T], [H2 | T2]} when H =:= H2 -> compare(T, T2);
+    _ -> false
+  end.
