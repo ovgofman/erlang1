e2b2978cea842735b78f575b4e2179f1ab793ca7
ovgofman <o.v.gofman@gmail.com>
2023-09-05 23:27:22 +0300

    Remove dependenies on helper_funcs.erl

--- lesson2_task10.erl
+++ lesson2_task10.erl
@@ -13,7 +13,26 @@
 -export([
   encode/1      % [a,a,a,b,c,c] -> [{3,a},{1,b},{2,c}]
 ]).
--import(helper_funcs, [le2ltec/3, first/1]).
+-import(lesson2_task05, [reverse/1]).
 
 %% @spec 10. [a,a,a,b,c,c] -> [{3,a},{1,b},{2,c}]
-encode(L) -> le2ltec(L, 0, first(L)).
+encode(L) ->
+  reverse(le2ltec(L, 0, first(L), [])).
+
+%% @spec Accumulates tuples of {Counter, Element} in the R list
+le2ltec(L, Counter, Elem, R) ->
+  case L of
+    [] -> R;
+    [H | []] when H =:= Elem -> [{Counter + 1, Elem} | R];
+    [H | T] when H =:= Elem -> le2ltec(T, Counter + 1, Elem, R);
+    [H | []] when H =/= Elem -> [H, {Counter, Elem} | R];
+    [H | T] when H =/= Elem, Counter =:= 1 -> le2ltec(T, 1, H, [Elem | R]);
+    [H | T] when H =/= Elem -> le2ltec(T, 1, H, [{Counter, Elem} | R])
+  end.
+
+%% @spec Returns Head of a L if it exists
+first(L) ->
+  case L of
+    [H | _] -> H;
+    _ -> L
+  end.
