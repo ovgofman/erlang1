b39fdb1ad87a775ae68e400b5097085c307b1cd8
ovgofman <o.v.gofman@gmail.com>
2023-09-05 23:29:00 +0300

    Remove dependenies on helper_funcs.erl

--- lesson2_task12.erl
+++ lesson2_task12.erl
@@ -15,7 +15,15 @@
 ]).
 -import(lesson2_task05, [reverse/1]).
 -import(lesson2_task07, [flatten/1]).
--import(helper_funcs, [ltec2le_mod/2]).
+-import(lesson2_task15, [replicate/2]).
 
 %% @spec 12. [{3,a},b,{2,c}] -> [a,a,a,b,c,c]
-decode_modified(L) -> reverse(flatten(ltec2le_mod(L, []))).
+decode_modified(L) ->
+  reverse(flatten(ltec2le_mod(L, []))).
+
+ltec2le_mod(L, R) ->
+  case L of
+    [] -> R;
+    [{A, E} | T] -> ltec2le_mod(T, [replicate([E], A) | R]);
+    [H | T] -> ltec2le_mod(T, [H | R])
+  end.
