dae2154d26e4173973d4b484b0e86687146a5453
ovgofman <o.v.gofman@gmail.com>
2023-09-05 23:28:04 +0300

    Remove dependenies on helper_funcs.erl

--- lesson2_task11.erl
+++ lesson2_task11.erl
@@ -13,7 +13,25 @@
 -export([
   encode_modified/1     % [a,a,a,b,c,c] -> [{3,a},b,{2,c}]
 ]).
--import(helper_funcs, [le2ltec_mod/3, first/1]).
+-import(lesson2_task05, [reverse/1]).
 
 %% @spec 11. [a,a,a,b,c,c] -> [{3,a},b,{2,c}]
-encode_modified(L) -> le2ltec_mod(L, 0, first(L)).
+encode_modified(L) ->
+  reverse(le2ltec(L, {0, first(L)}, [])).
+
+%% @spec Accumulates tuples of {Counter, Element} in the R list
+le2ltec(L, {Counter, Elem}, R) ->
+  case L of
+    [] -> R;
+    [H | []] when H =:= Elem -> [{Counter + 1, Elem} | R];
+    [H | T] when H =:= Elem -> le2ltec(T, {Counter + 1, Elem}, R);
+    [H | []] when H =/= Elem -> [{1, H}, {Counter, Elem} | R];
+    [H | T] when H =/= Elem -> le2ltec(T, {1, H}, [{Counter, Elem} | R])
+  end.
+
+%% @spec Returns Head of a L if it exists
+first(L) ->
+  case L of
+    [H | _] -> H;
+    _ -> L
+  end.
